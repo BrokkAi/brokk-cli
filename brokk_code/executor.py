e095b0bfb561412aa44ea3810790669fbc0d752a
Ryan SVIHLA <86598053+foundev@users.noreply.github.com>
2026-03-19 19:53:49 +0100

    chore: bump executor to 0.23.2.beta1 and package versions (#3151)

--- brokk_code/executor.py
+++ brokk_code/executor.py
@@ -19,7 +19,7 @@ from brokk_code.workspace import resolve_workspace_dir
 
 logger = logging.getLogger(__name__)
 
-BUNDLED_EXECUTOR_VERSION = "0.23.1.beta6"
+BUNDLED_EXECUTOR_VERSION = "0.23.2.beta1"
 _EXECUTOR_JAR_BASE_URL = "https://github.com/BrokkAi/brokk-releases/releases/download"
 _EXECUTOR_MAIN_CLASS = "ai.brokk.executor.HeadlessExecutorMain"
 _READY_SENTINEL = "Executor listening on http://"
